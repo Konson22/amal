5173953fb0bf631a3630143823ca1600bf8529f2
Konson Ak <konakech3@gmail.com>
2023-03-21 02:37:37 +0200

    add something

--- src/pages/Cause.jsx
+++ src/pages/Cause.jsx
@@ -3,13 +3,13 @@
 export default function Cause() {
   return (
     <>
-      <div className='causes-header h-[25vh] flex items-center justify-center text-white'>
-        <h1 className="text-6xl font-bold">Causes</h1>
+      <div className='causes-header lg:h-[25vh] lg:py-0 py-[2.5rem] flex items-center justify-center text-white'>
+        <h1 className="lg:text-6xl text-3xl lg:font-bold">Causes</h1>
       </div>
-      <div className="px-[8%] mt-4">
-        <div className="grid grid-cols-3 gap-6">
+      <div className="lg:px-[8%] px-3 mt-4">
+        <div className="lg:first-line:grid grid-cols-3 gap-6">
           {[...new Array(3)].map(context => (
-            <div className="border p-6">
+            <div className="border p-4">
               <p className="text-base">
                 Children in poverty are more likely to be exposed to the stress of violence, gangs and drugs. This can negatively affect brain development and long-term well-being. Your support during this vulnerable time.
               </p>
