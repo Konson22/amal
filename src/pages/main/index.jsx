7c06edfb36eba37ae9091fd683b54891ff7495b4
Konson Ak <konakech3@gmail.com>
2023-03-22 02:27:20 +0200

    style btn

--- src/pages/main/index.jsx
+++ src/pages/main/index.jsx
@@ -17,7 +17,7 @@ export default function Main() {
           <img className='lg:block hidden lg:h-[200px] w-[300px] rotate-[-12deg] border-4 border-amber-300 absolute top-[260px] right-[-70px]' src={process.env.PUBLIC_URL+'./images/ben-white-83tkHLPgg2Q-unsplash.jpg'} alt='' />
           <img className='lg:block hidden lg:h-[150px] w-[200px] rotate-[-12deg] border-4 border-amber-300 absolute top-[260px] left-[-70px]' src={process.env.PUBLIC_URL+'./images/siddhant-soni-CvqCzwVOhCM-unsplash.jpg'} alt='' />
         </div>
-        <div className="flex-1 lg:ml-24 pr-20">
+        <div className="flex-1 lg:ml-24 lg:pr-20">
           <h2 className="lg:text-5xl text-3xl font-bold mb-4">Helping is an act of Soft Heart</h2>
           <p>
             Regular donations are essential to our work, allowing us to plan ahead and operate efficiently.
@@ -26,7 +26,7 @@ export default function Main() {
             An inspiring addition to any kitchen.
             {/* When you sponsor a child you create a powerfull effect, multiplier effect. you help them see beyond the daily strugles of life in poverty. your support release them from a cycle of a short-tem descission and help them gain the ability to being contributing to thier families and communities. */}
           </p>
-          <Link className="block px-6 py-3 rounded text-yellow-700 bg-white font-semibold mt-6" to='/donate'>Make Donation</Link>
+          <Link className="inline-block px-6 py-3 rounded text-yellow-700 bg-white font-semibold mt-6" to='/donate'>Make Donation</Link>
         </div>
       </div>
       <BlogSection />
